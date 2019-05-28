07b3d688531df8d777d3b3fd377d8fe4645c55db
Rhys Arkins <rhys@arkins.net>
2019-05-28 16:22:06 +0200

    Ignore 6.2.0-next.0

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ export LOOKUP_NAME=npm
 export VERSION_SCHEME=npm
 export START_VERSION=5.0.0
 export IMAGE=npm
-# export IGNORED_VERSIONS=
+export IGNORED_VERSIONS=6.2.0-next.0
 
 npx renovatebot/dockerbuilder
